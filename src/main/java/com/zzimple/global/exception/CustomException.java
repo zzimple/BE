da5c6502b66effab2e96f94eb89b9dd0319d78e1
chaeelin <chaeee0123@gmail.com>
2025-05-20 17:06:55 +0900

    :sparkles: Feat: 공통 커스텀 예외 클래스 추가

---
+++ src/main/java/com/zzimple/global/exception/CustomException.java
@@ -0,0 +1,15 @@
+package com.zzimple.global.exception;
+
+import com.zzimple.global.exception.model.BaseErrorCode;
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+
+  private final BaseErrorCode errorCode;
+
+  public CustomException(BaseErrorCode errorCode) {
+    super(errorCode.getMessage());
+    this.errorCode = errorCode;
+  }
+}
