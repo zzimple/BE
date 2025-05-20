670a6c923feb79b8f53e6e448bb9b35de4cf3b89
chaeelin <chaeee0123@gmail.com>
2025-05-20 20:35:03 +0900

    :sparkles: FEAT: 출발지/도착지 임시 저장 응답 DTO 추가

---
+++ src/main/java/com/zzimple/estimate/dto/response/AddressDraftResponse.java
@@ -0,0 +1,20 @@
+package com.zzimple.estimate.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+@Schema(description = "출발지/도착지 - 묶음 Response")
+public class AddressDraftResponse {
+
+  @Schema(description = "도로명 주소", example = "서울특별시 강남구 테헤란로 223")
+  private String roadAddr;
+
+  @Schema(description = "응답 메세지", example = "도로명 주소 임시 저장이 완료되었습니다.")
+  private String message;
+
+}
