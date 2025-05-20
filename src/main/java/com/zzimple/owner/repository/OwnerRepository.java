1a4ea73ebdc4e65677d1efc25828f2550e9b8f26
chaeelin <chaeee0123@gmail.com>
2025-05-20 18:20:24 +0900

    :sparkles: Feat: [OWNER] 레포지토리 로직 추가

---
+++ src/main/java/com/zzimple/owner/repository/OwnerRepository.java
@@ -0,0 +1,10 @@
+package com.zzimple.owner.repository;
+
+import com.zzimple.owner.entity.Owner;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OwnerRepository extends JpaRepository<Owner, Long> {
+  // loginId 대신 businessNumber 로 조회
+  Optional<Owner> findByBusinessNumber(String businessNumber);
+}
