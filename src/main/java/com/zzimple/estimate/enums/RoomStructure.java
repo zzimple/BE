c42b8dc1e7a25b0a8d259a1ac9fe9847e566ccab
chaeelin <chaeee0123@gmail.com>
2025-05-22 01:12:03 +0900

    :sparkles: Feat: (주소)방 구조 Enum 정의

---
+++ src/main/java/com/zzimple/estimate/enums/RoomStructure.java
@@ -0,0 +1,13 @@
+package com.zzimple.estimate.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum RoomStructure {
+  ONE_ROOM,
+  ONE_HALF_ROOM,
+  TWO_ROOM,
+  THREE_ROOM,
+  FOUR_ROOM,
+  FIVE_OR_MORE;
+}
