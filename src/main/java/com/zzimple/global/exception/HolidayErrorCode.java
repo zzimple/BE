7e18d7f9057364e8aad62791a4682655defc97e3
chaeelin <chaeee0123@gmail.com>
2025-05-22 03:40:12 +0900

    :sparkles: Feat: 예외코드 추가

--- src/main/java/com/zzimple/global/exception/HolidayErrorCode.java
+++ src/main/java/com/zzimple/global/exception/HolidayErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum HolidayErrorCode implements BaseErrorCode {
 
-  API_CALL_FAILED("H001", "공휴일 API 호출에 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+  API_CALL_FAILED("H001", "공휴일 API 호출에 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
+  HOLIDAY_DRAFT_NOT_FOUND("H002", "저장된 공휴일 정보가 없습니다.", HttpStatus.NOT_FOUND);
 
   private final String code;
   private final String message;
