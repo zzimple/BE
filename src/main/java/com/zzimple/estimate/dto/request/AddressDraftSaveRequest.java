8875267f3f995ca7d313cd0060fdd653d3959e34
chaeelin <chaeee0123@gmail.com>
2025-05-22 01:15:55 +0900

    :recycle: Refactor: 주소 요청 필드를 AddressWithDetailRequest(주소+상세정보)로 교체

--- src/main/java/com/zzimple/estimate/dto/request/AddressDraftSaveRequest.java
+++ src/main/java/com/zzimple/estimate/dto/request/AddressDraftSaveRequest.java
@@ -10,9 +10,9 @@ import lombok.Setter;
 @NoArgsConstructor
 @Schema(description = "출발지/도착지 - 묶음 Request")
 public class AddressDraftSaveRequest {
-  @Schema(description = "현 집 주소")
-  private AddressDraftRequest fromAddress;
+  @Schema(description = "현 집 주소 + 상세 정보")
+  private AddressWithDetailRequest fromAddress;
 
-  @Schema(description = "이사할 곳 집 주소")
-  private AddressDraftRequest toAddress;
+  @Schema(description = "이사할 곳 집 주소 + 상세 정보")
+  private AddressWithDetailRequest toAddress;
 }
