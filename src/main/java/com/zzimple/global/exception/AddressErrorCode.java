22503e3263c7003fabaf5a528be48b1785163917
chaeelin <chaeee0123@gmail.com>
2025-05-22 03:32:01 +0900

    :sparkles: Feat: 예외코드 추가

--- src/main/java/com/zzimple/global/exception/AddressErrorCode.java
+++ src/main/java/com/zzimple/global/exception/AddressErrorCode.java
@@ -10,7 +10,9 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum AddressErrorCode implements BaseErrorCode {
 
-  ADDRESS_DRAFT_SAVE_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "E001", "임시 주소 저장 실패");
+  ADDRESS_DRAFT_NOT_FOUND(HttpStatus.NOT_FOUND, "A001", "임시 주소 정보가 존재하지 않습니다."),
+  JSON_PARSE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "A002", "Redis에 저장된 주소 정보를 파싱하는 데 실패했습니다."),
+  ADDRESS_DRAFT_SAVE_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "A003", "임시 주소 저장 실패");
 
   private final HttpStatus status;
   private final String code;
