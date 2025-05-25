c571cb2cea3dc2a69c93f1ea29ea567e59c0cb02
chaeelin <chaeee0123@gmail.com>
2025-05-26 00:46:02 +0900

    :sparkles: Feat: 공휴일 미리보기 & 이사 날짜 및 시간 & 이사 종류 및 옵션 key 추가

--- src/main/java/com/zzimple/global/config/RedisKeyUtil.java
+++ src/main/java/com/zzimple/global/config/RedisKeyUtil.java
@@ -12,7 +12,23 @@ public class RedisKeyUtil {
     return String.format("estimate:draft:%s:move-items", draftId);
   }
 
-  public static String draftHolidayKey(UUID draftId, String date) {
-    return String.format("estimate:draft:%s:holiday:%s", draftId, date);
+  public static String previewHolidayKey(String date) {
+    return String.format("estimate:preview:holiday:%s", date);
+  }
+
+  public static String draftMoveDateKey(UUID draftId) {
+    return String.format("estimate:draft:%s:move-date", draftId);
+  }
+
+  public static String draftMoveTimeKey(UUID draftId) {
+    return String.format("estimate:draft:%s:move-time", draftId);
+  }
+
+  public static String draftMoveTypeKey(UUID draftId) {
+    return "estimate:draft:" + draftId + ":moveType";
+  }
+
+  public static String draftMoveOptionKey(UUID draftId) {
+    return "draft:move-option:" + draftId;
   }
 }
