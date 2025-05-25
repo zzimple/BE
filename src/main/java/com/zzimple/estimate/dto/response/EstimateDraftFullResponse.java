704557500fa54a641ce4140e9144d491c22e5f87
chaeelin <chaeee0123@gmail.com>
2025-05-26 00:56:35 +0900

    :sparkles: Feat: 견적서 최종 불러오기 응답 DTO 추가

---
+++ src/main/java/com/zzimple/estimate/dto/response/EstimateDraftFullResponse.java
@@ -0,0 +1,22 @@
+package com.zzimple.estimate.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Schema(description = "견적서 최종 불러오기 응답")
+public class EstimateDraftFullResponse {
+  private AddressFullResponse address;
+  private HolidaySaveResponse holiday;
+  private MoveTypeResponse moveType;
+  private MoveItemsDraftResponse moveItems;
+  private MoveOptionTypeResponse smallMoveOption;
+}
