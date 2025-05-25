ed829906e6f90b58776581b088a1a6f436a46355
chaeelin <chaeee0123@gmail.com>
2025-05-26 00:58:10 +0900

    :sparkles: Feat: 주소 조회용 응답 DTO 추가

---
+++ src/main/java/com/zzimple/estimate/dto/response/AddressFullResponse.java
@@ -0,0 +1,20 @@
+package com.zzimple.estimate.dto.response;
+
+import com.zzimple.estimate.dto.request.AddressWithDetailRequest;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+@Schema(description = "주소 조회용 응답")
+public class AddressFullResponse {
+
+  @Schema(description = "출발지 주소 정보")
+  private AddressWithDetailRequest fromAddress;
+
+  @Schema(description = "도착지 주소 정보")
+  private AddressWithDetailRequest toAddress;
+}
