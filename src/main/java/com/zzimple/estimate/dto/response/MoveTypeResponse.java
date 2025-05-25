2789816d21ec8698dfe8a597a36edcb535dedc51
chaeelin <chaeee0123@gmail.com>
2025-05-26 00:48:36 +0900

    :sparkles: Feat: 이사 유형 임시 저장 응답 DTO 추가

---
+++ src/main/java/com/zzimple/estimate/dto/response/MoveTypeResponse.java
@@ -0,0 +1,16 @@
+package com.zzimple.estimate.dto.response;
+
+import com.zzimple.estimate.enums.MoveType;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@Schema(description = "이사 유형 저장 응답")
+public class MoveTypeResponse {
+
+  @Schema(description = "저장된 이사 유형", example = "HOME_MOVE")
+  private MoveType moveType;
+
+}
