37c1b7f6b8f210c852676bf89a4e13b2979b4287
chaeelin <chaeee0123@gmail.com>
2025-05-26 00:53:27 +0900

    :sparkles: Feat: 이사 날짜 임시 저장 요청 DTO 추가

---
+++ src/main/java/com/zzimple/estimate/dto/request/HolidaySaveRequest.java
@@ -0,0 +1,18 @@
+package com.zzimple.estimate.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Schema(description = "이사 날짜 저장 요청")
+public class HolidaySaveRequest {
+  @Schema(description = "저장 할 날짜")
+  private String date;
+
+  @Schema(description = "저장 할 시간")
+  private String time;
+}
