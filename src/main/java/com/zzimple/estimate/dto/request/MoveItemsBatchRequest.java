366cdfa31e01ebba160b9a23e1fe7e283e607c8a
chaeelin <chaeee0123@gmail.com>
2025-05-27 15:12:26 +0900

    :sparkles: Feat: 잔짐 박스 개수 & 고객 요청 사항 메모 추가

--- src/main/java/com/zzimple/estimate/dto/request/MoveItemsBatchRequest.java
+++ src/main/java/com/zzimple/estimate/dto/request/MoveItemsBatchRequest.java
@@ -18,4 +18,9 @@ public class MoveItemsBatchRequest {
   @Schema(description = "전체 박스 개수", example = "1")
   private Integer boxCount;
 
+  @Schema(description = "잔짐 박스 개수", example = "1")
+  private int leftoverBoxCount;
+
+  @Schema(description = "고객 요청사항 메모")
+  private String requestNote;
 }
