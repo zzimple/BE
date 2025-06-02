aaa5b4e947700e98efcf762de7c0b8ba9d660081
chaeelin <chaeee0123@gmail.com>
2025-06-02 13:40:14 +0900

    :sparkles: Feat: 견적서 물품 관련 레포지토리 추가

---
+++ src/main/java/com/zzimple/estimate/guest/repository/MoveItemsRepository.java
@@ -0,0 +1,9 @@
+package com.zzimple.estimate.guest.repository;
+
+import com.zzimple.estimate.guest.entity.MoveItems;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MoveItemsRepository extends JpaRepository<MoveItems, Long> {
+  List<MoveItems> findByEstimateNo(Long estimateNo);
+}
