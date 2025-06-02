9d6df44993e3315632835b2803dcdce984791798
chaeelin <chaeee0123@gmail.com>
2025-06-02 13:39:53 +0900

    :sparkles: Feat: 사장님 물품 추가 금액 관련 레포지토리 추가

---
+++ src/main/java/com/zzimple/estimate/owner/repository/MoveItemExtraChargeRepository.java
@@ -0,0 +1,9 @@
+package com.zzimple.estimate.owner.repository;
+
+import com.zzimple.estimate.owner.entity.MoveItemExtraCharge;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MoveItemExtraChargeRepository extends JpaRepository<MoveItemExtraCharge, Long> {
+  List<MoveItemExtraCharge> findByMoveItemId(Long moveItemId);
+}
