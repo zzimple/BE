6925e4aaed932e360362b553e073676173cb558a
chaeelin <chaeee0123@gmail.com>
2025-06-02 13:38:20 +0900

    :sparkles: Feat: 사장님 물품 기본 금액 저장 및 불러오기 레포지토리 추가

---
+++ src/main/java/com/zzimple/estimate/owner/repository/MoveItemBasePriceRepository.java
@@ -0,0 +1,11 @@
+package com.zzimple.estimate.owner.repository;
+
+import com.zzimple.estimate.owner.entity.MoveItemBasePrice;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MoveItemBasePriceRepository extends JpaRepository<MoveItemBasePrice, Long> {
+  Optional<MoveItemBasePrice> findByStoreIdAndItemTypeId(Long storeId, Long itemTypeId);
+  List<MoveItemBasePrice> findByStoreIdAndItemTypeIdIn(Long storeId, List<Long> itemTypeIds);
+}
