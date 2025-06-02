c78965ada76bbd6f6fe2155026ea11dc927062f2
chaeelin <chaeee0123@gmail.com>
2025-06-02 11:30:42 +0900

    :recycle: Refactor: end-point 변경

--- src/main/java/com/zzimple/global/jwt/AuthController.java
+++ src/main/java/com/zzimple/global/jwt/AuthController.java
@@ -21,7 +21,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/users")
 @RequiredArgsConstructor
 @Slf4j
 @Tag(name = "User")
