123ccf9ff555ab7e2e08bdb931f7e795e35922bc
chaeelin <chaeee0123@gmail.com>
2025-06-03 16:55:07 +0900

    :sparkles: Feat: findByOwnerId 추가

--- src/main/java/com/zzimple/owner/store/repository/StoreRepository.java
+++ src/main/java/com/zzimple/owner/store/repository/StoreRepository.java
@@ -1,7 +1,10 @@
 package com.zzimple.owner.store.repository;
 
 import com.zzimple.owner.store.entity.Store;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
+  Optional<Store> findByOwnerId(Long ownerId);  // ownerId는 FK 아님, 인덱스로 검색
 }
+
