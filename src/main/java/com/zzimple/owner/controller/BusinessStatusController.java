839c73a3acacce3ac7956d898984400ae5738549
chaeelin <chaeee0123@gmail.com>
2025-06-03 16:14:41 +0900

    :recycle: Refactor: 엔드포인트 변경

--- src/main/java/com/zzimple/owner/controller/BusinessStatusController.java
+++ src/main/java/com/zzimple/owner/controller/BusinessStatusController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/owner/business")
+@RequestMapping("/owner/business")
 @RequiredArgsConstructor
 public class BusinessStatusController {
 
--- src/main/java/com/zzimple/staff/controller/StaffController.java
+++ src/main/java/com/zzimple/staff/controller/StaffController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/staff")
+@RequestMapping("/staff")
 @RequiredArgsConstructor
 @Slf4j
 public class StaffController {
