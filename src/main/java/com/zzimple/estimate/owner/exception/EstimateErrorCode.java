3e6e9491af7bfa3a14058c23d87987efb74090e2
chaeelin <chaeee0123@gmail.com>
2025-06-03 16:44:34 +0900

    :sparkles: Feat: 에러코드 추가

--- src/main/java/com/zzimple/estimate/owner/exception/EstimateErrorCode.java
+++ src/main/java/com/zzimple/estimate/owner/exception/EstimateErrorCode.java
@@ -15,7 +15,12 @@ public enum EstimateErrorCode implements BaseErrorCode {
   ESTIMATE_NOT_FOUND("EST004", "견적서를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
   INVALID_MOVE_OPTION("EST005", "이사 옵션이 유효하지 않습니다.", HttpStatus.BAD_REQUEST),  // ← 추가
   STORE_PRICE_SETTING_NOT_FOUND("EST006", "사장님의 단가 설정이 존재하지 않습니다.", HttpStatus.NOT_FOUND),
-  ESTIMATE_ACCESS_DENIED("EST007", "해당 견적서에 접근할 수 없습니다.", HttpStatus.FORBIDDEN);
+  ESTIMATE_ACCESS_DENIED("EST007", "해당 견적서에 접근할 수 없습니다.", HttpStatus.FORBIDDEN),
+
+  ESTIMATE_ALREADY_FINALIZED("EST008", "이미 확정된 견적서입니다.", HttpStatus.CONFLICT),
+  INVALID_ESTIMATE_ITEMS("EST009", "견적 항목 정보가 잘못되었습니다.", HttpStatus.BAD_REQUEST),
+  INVALID_EXTRA_CHARGE("EST010", "추가금 항목이 유효하지 않습니다.", HttpStatus.BAD_REQUEST),
+  ESTIMATE_STORE_MISMATCH("EST011", "사장님 정보가 견적서와 일치하지 않습니다.", HttpStatus.FORBIDDEN);
 
 
   private final String code;
