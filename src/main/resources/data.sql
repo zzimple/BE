c70b2bc63f4c592f5e76ad0fb55e646450ac565a
chaeelin <chaeee0123@gmail.com>
2025-06-07 15:02:44 +0900

    :sparkles: Feat: 가구,가전,기타 목록들 DB에 저장

---
+++ src/main/resources/data.sql
@@ -0,0 +1,44 @@
+-- 가구 (FURNITURE)
+INSERT INTO item_type (item_type_id, item_type_name, category) VALUES (1001, '침대', 'FURNITURE');
+INSERT INTO item_type (item_type_id, item_type_name, category) VALUES (1002, '쇼파', 'FURNITURE');
+INSERT INTO item_type (item_type_id, item_type_name, category) VALUES (1003, '옷장-단품', 'FURNITURE');
+INSERT INTO item_type (item_type_id, item_type_name, category) VALUES (1004, '옷장-연결장', 'FURNITURE');
+INSERT INTO item_type (item_type_id, item_type_name, category) VALUES (1005, '행거', 'FURNITURE');
+INSERT INTO item_type (item_type_id, item_type_name, category) VALUES (1006, '시스템 행거', 'FURNITURE');
+INSERT INTO item_type (item_type_id, item_type_name, category) VALUES (1007, '화장대', 'FURNITURE');
+INSERT INTO item_type (item_type_id, item_type_name, category) VALUES (1008, '수납장/서랍장', 'FURNITURE');
+INSERT INTO item_type (item_type_id, item_type_name, category) VALUES (1009, '진열장', 'FURNITURE');
+INSERT INTO item_type (item_type_id, item_type_name, category) VALUES (1010, '선반', 'FURNITURE');
+INSERT INTO item_type (item_type_id, item_type_name, category) VALUES (1011, '거실장/TV장', 'FURNITURE');
+INSERT INTO item_type (item_type_id, item_type_name, category) VALUES (1012, '책장', 'FURNITURE');
+INSERT INTO item_type (item_type_id, item_type_name, category) VALUES (1013, '책상', 'FURNITURE');
+INSERT INTO item_type (item_type_id, item_type_name, category) VALUES (1014, '테이블/식탁', 'FURNITURE');
+INSERT INTO item_type (item_type_id, item_type_name, category) VALUES (1015, '의자', 'FURNITURE');
+
+-- 가전 (APPLIANCE)
+INSERT INTO item_type (item_type_id, item_type_name, category) VALUES (2001, 'TV', 'APPLIANCE');
+INSERT INTO item_type (item_type_id, item_type_name, category) VALUES (2002, '모니터', 'APPLIANCE');
+INSERT INTO item_type (item_type_id, item_type_name, category) VALUES (2003, 'PC/데스크탑', 'APPLIANCE');
+INSERT INTO item_type (item_type_id, item_type_name, category) VALUES (2004, '세탁기', 'APPLIANCE');
+INSERT INTO item_type (item_type_id, item_type_name, category) VALUES (2005, '건조기', 'APPLIANCE');
+INSERT INTO item_type (item_type_id, item_type_name, category) VALUES (2006, '청소기', 'APPLIANCE');
+INSERT INTO item_type (item_type_id, item_type_name, category) VALUES (2007, '의류관리기', 'APPLIANCE');
+INSERT INTO item_type (item_type_id, item_type_name, category) VALUES (2008, '냉장고', 'APPLIANCE');
+INSERT INTO item_type (item_type_id, item_type_name, category) VALUES (2009, '전자레인지', 'APPLIANCE');
+INSERT INTO item_type (item_type_id, item_type_name, category) VALUES (2010, '가스레인지', 'APPLIANCE');
+INSERT INTO item_type (item_type_id, item_type_name, category) VALUES (2011, '정수기', 'APPLIANCE');
+INSERT INTO item_type (item_type_id, item_type_name, category) VALUES (2012, '에어컨', 'APPLIANCE');
+INSERT INTO item_type (item_type_id, item_type_name, category) VALUES (2013, '공기청정기', 'APPLIANCE');
+INSERT INTO item_type (item_type_id, item_type_name, category) VALUES (2014, '선풍기', 'APPLIANCE');
+INSERT INTO item_type (item_type_id, item_type_name, category) VALUES (2015, '안마의자', 'APPLIANCE');
+
+-- 기타 (OTHER)
+INSERT INTO item_type (item_type_id, item_type_name, category) VALUES (3001, '거울', 'OTHER');
+INSERT INTO item_type (item_type_id, item_type_name, category) VALUES (3002, '커튼', 'OTHER');
+INSERT INTO item_type (item_type_id, item_type_name, category) VALUES (3003, '빨래 건조대', 'OTHER');
+INSERT INTO item_type (item_type_id, item_type_name, category) VALUES (3004, '캐리어', 'OTHER');
+INSERT INTO item_type (item_type_id, item_type_name, category) VALUES (3005, '운동용품', 'OTHER');
+INSERT INTO item_type (item_type_id, item_type_name, category) VALUES (3006, '책', 'OTHER');
+INSERT INTO item_type (item_type_id, item_type_name, category) VALUES (3007, '화분', 'OTHER');
+INSERT INTO item_type (item_type_id, item_type_name, category) VALUES (3008, '비데', 'OTHER');
+INSERT INTO item_type (item_type_id, item_type_name, category) VALUES (3009, '조명기구', 'OTHER');
