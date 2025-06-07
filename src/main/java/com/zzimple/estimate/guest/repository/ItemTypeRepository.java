d4459283db019f77220e060ad4ed40a11f5b5a77
chaeelin <chaeee0123@gmail.com>
2025-06-07 15:14:23 +0900

    :sparkles: Feat: 짐 목록 레포 추가

---
+++ src/main/java/com/zzimple/estimate/guest/repository/ItemTypeRepository.java
@@ -0,0 +1,8 @@
+package com.zzimple.estimate.guest.repository;
+
+import com.zzimple.estimate.guest.entity.ItemType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemTypeRepository extends JpaRepository<ItemType, Long> {
+}
+
