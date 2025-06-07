a5da42e61120fee608c93f3286767324428c0ef3
chaeelin <chaeee0123@gmail.com>
2025-06-07 14:44:30 +0900

    :sparkles: Feat: 짐 아이디로 삭제 추가

--- src/main/java/com/zzimple/estimate/owner/repository/MoveItemExtraChargeRepository.java
+++ src/main/java/com/zzimple/estimate/owner/repository/MoveItemExtraChargeRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MoveItemExtraChargeRepository extends JpaRepository<MoveItemExtraCharge, Long> {
   List<MoveItemExtraCharge> findByMoveItemId(Long moveItemId);
+  void deleteByMoveItemId(Long moveItemId);
 }
