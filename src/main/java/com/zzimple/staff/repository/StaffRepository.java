9b7e469a9348fc038ed4374641cd8d65697e5a5b
chaeelin <chaeee0123@gmail.com>
2025-06-07 15:06:29 +0900

    :recycle: Refactor: staffId -> userId + 사장님Id로 찾기 추가

--- src/main/java/com/zzimple/staff/repository/StaffRepository.java
+++ src/main/java/com/zzimple/staff/repository/StaffRepository.java
@@ -1,11 +1,12 @@
 package com.zzimple.staff.repository;
 
 import com.zzimple.staff.entity.Staff;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StaffRepository extends JpaRepository<Staff, Long> {
-  Optional<Staff> findByStaffId(Long staffId);
-  boolean existsByStaffIdAndOwnerId(Long staffId, Long ownerId);
-
+  Optional<Staff> findByUserId(Long userId);
+  boolean existsByUserIdAndOwnerId(Long userId, Long ownerId);
+  List<Staff> findByOwnerId(Long ownerId);
 }
