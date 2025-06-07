5b39f813164e84004f0c8f195c21515ce3ef3f5e
chaeelin <chaeee0123@gmail.com>
2025-06-07 14:28:25 +0900

    :recycle: Refactor: 에러 코드 추가 및 코드 번호 수정

--- src/main/java/com/zzimple/owner/exception/BusinessErrorCode.java
+++ src/main/java/com/zzimple/owner/exception/BusinessErrorCode.java
@@ -9,10 +9,10 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum BusinessErrorCode implements BaseErrorCode {
 
-  BUSINESS_NUMBER_MISSING("B001", "사업자등록번호가 누락되었습니다.", HttpStatus.BAD_REQUEST),
-  BUSINESS_NUMBER_INVALID("B002", "유효하지 않은 사업자등록번호입니다.", HttpStatus.BAD_REQUEST),
-  BUSINESS_NUMBER_ALREADY_EXISTS("B003", "이미 존재하는 사업자등록번호입니다.", HttpStatus.CONFLICT);
+  BUSINESS_NUMBER_MISSING("B4041", "사업자등록번호가 누락되었습니다.", HttpStatus.BAD_REQUEST),
+  BUSINESS_NUMBER_INVALID("B4042", "유효하지 않은 사업자등록번호입니다.", HttpStatus.BAD_REQUEST),
 
+  BUSINESS_NUMBER_ALREADY_EXISTS("B4091", "이미 존재하는 사업자등록번호입니다.", HttpStatus.CONFLICT);
 
   private final String code;
   private final String message;
