e35c475ead8bd00af47cc0dbc5163c04ca743d22
chaeelin <chaeee0123@gmail.com>
2025-06-07 14:51:57 +0900

    :recycle: Refactor: 짐 총 금액 응답 추가

--- src/main/java/com/zzimple/estimate/owner/dto/response/ItemTotalResponse.java
+++ src/main/java/com/zzimple/estimate/owner/dto/response/ItemTotalResponse.java
@@ -14,8 +14,21 @@ import lombok.Setter;
 @AllArgsConstructor
 @Schema(description = "각 이사 항목의 기본 금액, 추가금, 총 금액 정보를 담은 응답 DTO")
 public class ItemTotalResponse {
+  @Schema(description = "MoveItems 엔티티 ID")
   private Long itemId;
+
+  @Schema(description = "아이템 종류 ID (item_type 테이블 참조)")
+  private Long itemTypeId;
+
+  @Schema(description = "아이템 이름 (item_type 테이블 참조)")
+  private String itemTypeName;
+
+  @Schema(description = "기본 금액 총합")
   private int baseTotal;
+
+  @Schema(description = "추가 금액 총합")
   private int extraTotal;
+
+  @Schema(description = "항목 총 금액 (기본 + 추가)")
   private int itemTotal;
 }
