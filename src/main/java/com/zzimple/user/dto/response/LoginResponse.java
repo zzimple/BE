327a33ead67af1f20b5aed6f3a10145e842fcaf5
chaeelin <chaeee0123@gmail.com>
2025-06-07 14:43:03 +0900

    :recycle: Refactor: 리프레시 토큰 응답 제거

--- src/main/java/com/zzimple/user/dto/response/LoginResponse.java
+++ src/main/java/com/zzimple/user/dto/response/LoginResponse.java
@@ -11,6 +11,6 @@ public class LoginResponse {
   @Schema(description = "JWT 액세스 토큰")
   private String accessToken; // JWT 액세스 토큰
 
-  @Schema(description = "JWT 리프레시 토큰")
-  private String refreshToken; // JWT 리프레시 토큰 (필요한 경우)
+//  @Schema(description = "JWT 리프레시 토큰")
+//  private String refreshToken; // JWT 리프레시 토큰 (필요한 경우)
 }
