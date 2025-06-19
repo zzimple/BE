dd47079ea8f13a9951cbaa4e7c766e9ff4577ade
chaeelin <chaeee0123@gmail.com>
2025-06-20 07:44:13 +0900

    :sparkles: Feat: 사장님 견적서 현황 - summary

--- src/main/java/com/zzimple/estimate/owner/service/EstimateConfirmedService.java
+++ src/main/java/com/zzimple/estimate/owner/service/EstimateConfirmedService.java
@@ -2,7 +2,17 @@ package com.zzimple.estimate.owner.service;
 
 import com.zzimple.estimate.guest.enums.EstimateStatus;
 import com.zzimple.estimate.owner.dto.response.EstimateConfirmedResponse;
+import com.zzimple.estimate.owner.dto.response.EstimateSummaryResponse;
 import com.zzimple.estimate.owner.repository.EstimateRepository;
+import com.zzimple.global.exception.CustomException;
+import com.zzimple.owner.entity.Owner;
+import com.zzimple.owner.repository.OwnerRepository;
+import com.zzimple.owner.store.entity.Store;
+import com.zzimple.owner.store.exception.StoreErrorCode;
+import com.zzimple.owner.store.repository.StoreRepository;
+import com.zzimple.user.entity.User;
+import com.zzimple.user.exception.UserErrorCode;
+import com.zzimple.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -18,11 +28,39 @@ import org.springframework.stereotype.Service;
 public class EstimateConfirmedService {
 
   private final EstimateRepository estimateRepository;
+  private final StoreRepository storeRepository;
+  private final UserRepository userRepository;
+  private final OwnerRepository ownerRepository;
 
   // 아예 견적서 확정된 코드
   public Page<EstimateConfirmedResponse> getConfirmedEstimates(Long storeId, int page, int size) {
     Pageable pageable = PageRequest.of(page, size, Sort.by("createdAt").descending());
     return estimateRepository.findByStoreIdAndStatus(storeId, EstimateStatus.CONFIRMED, pageable)
-        .map(EstimateConfirmedResponse::from);
+        .map(estimate -> {
+          Long userId = estimate.getUserId();
+          String guestName = userRepository.findById(userId)
+              .map(User::getUserName)
+              .orElseThrow(() -> new CustomException(UserErrorCode.USER_NOT_FOUND));
+
+          return EstimateConfirmedResponse.from(estimate, guestName);
+        });
+  }
+
+  // 확정 / 진행 / 수락
+  public EstimateSummaryResponse getEstimateSummary(Long userId) {
+    log.info("🟡 [getEstimateSummary] 요청 유저 ID: {}", userId);
+
+    Store store = storeRepository.findByOwnerUserId(userId)
+        .orElseThrow(() -> new CustomException(StoreErrorCode.STORE_NOT_FOUND));
+
+    Long storeId = store.getId();
+    log.info("🟢 매핑된 Store ID: {}", storeId);
+
+
+    int confirmedCount = estimateRepository.countByStoreIdAndStatus(storeId, EstimateStatus.CONFIRMED);
+    int completedCount = estimateRepository.countByStoreIdAndStatus(storeId, EstimateStatus.COMPLETED);
+    int inProgressCount = confirmedCount - completedCount; // 진행 중인 견적서
+
+    return new EstimateSummaryResponse(confirmedCount, completedCount, inProgressCount);
   }
 }
