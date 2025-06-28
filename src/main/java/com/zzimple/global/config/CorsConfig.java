3fe6a9f74433c1ff96d4996e9f36176ff3b69ca6
chaeelin <chaeee0123@gmail.com>
2025-06-28 19:51:27 +0900

    :bug: Fix: CORS 버그 수정

--- src/main/java/com/zzimple/global/config/CorsConfig.java
+++ src/main/java/com/zzimple/global/config/CorsConfig.java
@@ -20,7 +20,7 @@ public class CorsConfig {
     // 환경 변수에 정의된 출처만 허용
     configuration.setAllowedOrigins(Arrays.asList(allowedOrigins));
     // 리스트에 작성한 HTTP 메소드 요청만 허용
-    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH"));
+    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
     // 리스트에 작성한 헤더들이 포함된 요청만 허용
     configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
     // 쿠키나 인증 정보를 포함하는 요청 허용
